2fda8751c164c8b864acbdf76dc1d9a80b61e15e
Barkey1201 <51892022+Barkey1201@users.noreply.github.com>
2021-05-24 17:52:09 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,14 @@
+const panels = document.querySelectorAll('.panel')
 
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')  
+    } )
+})
 
-$("button").on("click", function(){
-    $("h1").animate({opacity:0.5});
-});
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
